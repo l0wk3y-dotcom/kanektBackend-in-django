d8420b0dcd6993758ac767567701268224200bde
l0wk3y <149136576+l0wk3y-dotcom@users.noreply.github.com>
2024-12-26 13:01:36 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -133,18 +133,31 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
+import dj_database_url
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        #'ENGINE': 'django.db.backends.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'railway',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'XqSpoqKfSpUwZZhmhdcxTAWbiwjxMBFK',
+
+        'HOST': 'postgres.railway.internal',
+
+        'PORT': '5432',
+
     }
+
 }
-
-
+DATABASES["default"] = dj_database_url.parse("postgresql://postgres:XqSpoqKfSpUwZZhmhdcxTAWbiwjxMBFK@junction.proxy.rlwy.net:23579/railway")
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
